03ab70a642c4169eb8d549e163f5d7fdda7eb86e
Mitchell Johnstone <mitchell.johnstone@directsupply.com>
2024-12-19 13:03:13 -0600

    Remove to make the local deploy work w/ react

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/marcus-movie-day',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/marcus-movie-day' : '',
 };
 
 export default nextConfig;
